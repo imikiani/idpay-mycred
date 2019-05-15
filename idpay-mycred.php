50266ba49b80a38abede4a3ad58625866bc31717
Imi Kiani <kianiman@gmail.com>
2019-05-15 11:08:37 +0430

    load text domain

--- idpay-mycred.php
+++ idpay-mycred.php
@@ -14,4 +14,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+/**
+ * Load plugin textdomain.
+ *
+ * @since 1.0.0
+ */
+function idpay_mycred_load_textdomain() {
+	load_plugin_textdomain( 'idpay-mycred', FALSE, basename( dirname( __FILE__ ) ) . '/languages' );
+}
+
+add_action( 'init', 'idpay_mycred_load_textdomain' );
+
 require_once( plugin_dir_path( __FILE__ ) . 'class-mycred-gateway-idpay.php' );
